c52720a1b24dab8c5ec7410ad32b47f5b1e16f5f
CrypticHushane <oshanewilliams99@outlook.com>
2021-08-22 07:14:03 -0500

    added roles and role_users table

---
+++ database/migrations/2021_08_22_120351_create_roles_user_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles_user', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('roles_user', function (Blueprint $table){
+            $table->dropForeign('user_id');
+            $table->dropForeign('role_id');
+        });
+        Schema::dropIfExists('roles_user');
+    }
+}
